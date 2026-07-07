2ff5771322ae96b24e623e8d1670b6bcbb55e2f3
Yash Patel <communicate2yashpatel@gmail.com>
2026-07-07 23:24:36 +0530

    loops

---
+++ loops.js
@@ -0,0 +1,31 @@
+// loops 
+
+//! for loop : 
+// when we know how many times exicution is occur then use for loop
+// syntax : for(initialization ; condition ; increment/decrement){}
+
+// ex : 
+//for (let i=0;i<5;i++){
+//    console.log(i);
+//}
+
+
+//! while loop: 
+// when we don't know howw many time exicution is occur then use while loop
+// syntax : while(condition){
+//  code 
+//  increment/decrement
+//}
+
+// ex : 
+/*let i = 0;
+while (i<=5) {
+    console.log(i);
+    i++;
+}*/
+
+//! do - while loop
+// when we require code is exicution at-least once then use do while loop
+// syntax : do{
+//  code
+// }while(condition);
